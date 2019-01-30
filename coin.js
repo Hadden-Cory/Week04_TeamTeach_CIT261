420fdb4a9f0f81897418ce2a9e7e39b3c209df7f
Mona Lisa <had17007@byui.edu>
2019-01-30 13:18:19 -0700

    Intial Files
    Intial Files

---
+++ coin.js
@@ -0,0 +1,5 @@
+function randomToss() {
+    //  var num = Math.floor((Math.random() * 2000) + 300);
+   document.getElementById("goldDollar").classList.toggle("cardFliping")
+   document.getElementById("goldDollarAction").classList.toggle("cardFliping")
+}
